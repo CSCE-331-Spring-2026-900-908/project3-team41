6360db4b4e09bc8f297c1dc2e6ab981aeee1b7c6
Kamron Lindberg <kllind25@gmail.com>
2026-03-30 11:56:42 -0500

    Connected all manager pages with sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Portal from "./pages/portal";
 import CashierLoginPage from "./pages/cashier/CashierLoginPage";
 import CashierPage from "./pages/cashier/CashierPage";
 import ManagerLoginPage from "./pages/manager/ManagerLoginPage";
-import AnalyticsPage from "./pages/manager/AnalyticsPage";
+import ManagerNav from "./pages/manager/ManagerNav";
 
 export default function App() {
   return (
@@ -14,7 +14,8 @@ export default function App() {
         <Route path="/cashier/login" element={<CashierLoginPage />} />
         <Route path="/cashier" element={<CashierPage />} />
         <Route path="/manager/login" element={<ManagerLoginPage />} />
-        <Route path="/manager" element={<AnalyticsPage />} />
+        <Route path="/manager" element={<Navigate to="/manager/analytics" replace />} />
+        <Route path="/manager/:page" element={<ManagerNav />} />
       </Routes>
     </BrowserRouter>
   );
