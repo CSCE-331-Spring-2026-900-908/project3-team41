d69b21f247042f1d326f63f78a469ea0077bd57d
Benjamin Roesler <bjroesler21@tamu.edu>
2026-03-30 09:22:57 -0500

    added menu page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Portal from "./pages/portal";
+import MenuPage from "./pages/menu/MenuPage";
 import CashierLoginPage from "./pages/cashier/CashierLoginPage";
 import CashierPage from "./pages/cashier/CashierPage";
 import ManagerLoginPage from "./pages/manager/ManagerLoginPage";
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Portal />} />
         <Route path="/login" element={<Navigate to="/cashier/login" replace />} />
         <Route path="/cashier/login" element={<CashierLoginPage />} />
+        <Route path="/menu" element={<MenuPage />} />
         <Route path="/cashier" element={<CashierPage />} />
         <Route path="/manager/login" element={<ManagerLoginPage />} />
         <Route path="/manager" element={<AnalyticsPage />} />
